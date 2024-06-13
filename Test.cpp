b74a9d111bfaea176597d45bd61520acd270c2d8
Hadarfro <122720878+Hadarfro@users.noreply.github.com>
2024-06-13 14:14:37 +0300

    creating tests

---
+++ Test.cpp
@@ -0,0 +1,15 @@
+#include "doctest.h"
+#include "player.hpp"
+#include "board.hpp"
+#include "catan.hpp"
+
+/*
+ * Author: Hadar Froimowich.
+ * ID: 213118458
+ * Email: hadarfro12@gmail.com
+ */
+
+using namespace std;
+using namespace doctest;
+
+
